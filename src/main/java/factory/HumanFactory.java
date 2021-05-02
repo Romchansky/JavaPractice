de15db89f50539a086ae70aaa962c83735d36ef8
Romchansky <romkapetrenko8@gmail.com>
2021-05-02 22:31:11 +0300

    create classes elf

---
+++ src/main/java/factory/HumanFactory.java
@@ -0,0 +1,23 @@
+package factory;
+
+public class HumanFactory extends RaceFactory {
+    @Override
+    public Archer createArcher() {
+        return null;
+    }
+
+    @Override
+    public Rider createRider() {
+        return null;
+    }
+
+    @Override
+    public Warrior createWarrior() {
+        return null;
+    }
+
+    @Override
+    public Wizard createWizard() {
+        return null;
+    }
+}
