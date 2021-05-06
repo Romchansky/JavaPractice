c8e8d569580841361cd5767619a3be30abce317d
Romchansky <romkapetrenko8@gmail.com>
2021-05-06 22:26:53 +0300

    new commit after merge

--- src/main/java/factory/Main.java
+++ src/main/java/factory/Main.java
@@ -2,5 +2,17 @@ package factory;
 
 public class Main {
 
-    public final static String SELECTED_RACE = "Elf";
+    public final static String SELECTED_RACE = "Ork";
+
+    public static void main(String[] args) {
+
+        RaceFactory raceFactory = RaceFactory.of();
+
+        raceFactory.createWarrior().saveWarrior();
+        raceFactory.createRider().saveRider();
+        raceFactory.createArcher().saveArcher();
+        raceFactory.createWizard().saveWizard();
+    }
+
+
 }
