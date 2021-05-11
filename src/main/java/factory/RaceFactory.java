f122b5478e917dc27363aea63bd46a0cab48e76d
Romchansky <romkapetrenko8@gmail.com>
2021-05-11 20:50:55 +0300

    changes java version in pom.xml

--- src/main/java/factory/RaceFactory.java
+++ src/main/java/factory/RaceFactory.java
@@ -5,8 +5,6 @@ import factory.elf.ElfFactory;
 import factory.human.HumanFactory;
 import factory.ork.OrkFactory;
 
-import static factory.Main.SELECTED_RACE;
-
 public abstract class RaceFactory {
 
     private static RaceFactory race;
@@ -14,18 +12,18 @@ public abstract class RaceFactory {
     protected RaceFactory() {};
 
     public static RaceFactory of() {
-        if(race == null ) race = selectCharacter(SELECTED_RACE);
+        if(race == null ) race = selectCharacter();
         return race;
     }
 
-    private static RaceFactory selectCharacter(String race) {
-        switch (race) {
-            case "Ork": return new OrkFactory();
-            case "Elf": return new ElfFactory();
-            case "Dwarf": return new DwarfFactory();
-            case "Human": return new HumanFactory();
-            default: throw new RuntimeException("This race is not found");
-        }
+    private static RaceFactory selectCharacter() {
+        return switch (Main.SELECTED_RACE) {
+            case "Ork" -> new OrkFactory();
+            case "Elf" -> new ElfFactory();
+            case "Dwarf" -> new DwarfFactory();
+            case "Human" -> new HumanFactory();
+            default -> throw new RuntimeException("This race is not found");
+        };
     }
 
     public abstract Archer createArcher();
